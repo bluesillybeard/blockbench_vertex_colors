a5a6e56af9d5d048a8e5e33435e87a2bfb60ba6d
bluesillybeard <bluesillybeard2@gmail.com>
2026-02-15 14:38:26 -0700

    Initialized and stored vertex data (seemingly) correctly

--- TODO.md
+++ TODO.md
@@ -2,8 +2,6 @@
 
 ## Progress Checklist
 
-- add vertex color & mix factor into vertex data
-    - make sure it is saved into the bbmodel file
 - add "vertex color" and "texture + vertex color" preview display modes
 - add vertex color paint tool to the paint workspace
 - add export options that include the vertex color data
--- vertex_colors.js
+++ vertex_colors.js
@@ -1,14 +1,24 @@
-let vertex_color_property;
+const VERTEX_COLOR_DATA_NAME = "data_vertex_colors"
 
-function onaddmesh() {
-    console.log("The user added a mesh");
-    Mesh.all.forEach(m => {
-        m.forAllFaces((face, key) => {
-            console.log("Face " + key + " had vertex colors " + face["data_vertex_colors"]);
-            face["data_vertex_colors"] = "hello";
-        })
-    })
-}
+// indices into the vertex color list.
+// In the future this may be replaced with an actual class.
+// TODO: in fact maybe it should be converted into a class, the only issue with that is it's not POD anymore and needs custom json serialization logic I think.
+const RED = 0
+const GREEN = 1
+const BLUE = 2
+const ALPHA = 3
+const MIX = 4
+
+// "error code" Magenta with a mix factor of zero (fully textured, no vertex color applied to preview)
+// TODO: switch this to black
+const DEFAULT_VERTEX_COLOR = [1.0, 0.0, 1.0, 1.0, 0.0]
+
+// The property for vertex colors so they are saved and loaded to/from the bbmodel file correctly.
+let vertex_color_property
+
+// Old versions of monkey patched functions
+let old_mesh_face_extend
+let old_meshFace_getSaveCopy
 
 Plugin.register('vertex_colors', {
     title: 'Vertex Colors',
@@ -19,24 +29,69 @@ Plugin.register('vertex_colors', {
     variant: 'both',
     await_loading: 'true',
     onload() {
-        console.log("Hello I am a vertex plugin");
-        vertex_color_property = new Property(Face, "string", "data_vertex_colors", {
-            default: "hi",
+        vertex_color_property = new Property(MeshFace, "object", VERTEX_COLOR_DATA_NAME, {
+            default: {},
             merge: (instance, data) => {
-                instance["data_vertex_colors"] = structuredClone(data["data_vertex_colors"])
-                console.log("vertex color merged")
+                instance[VERTEX_COLOR_DATA_NAME] = structuredClone(data[VERTEX_COLOR_DATA_NAME])
             },
             copy_value: (instance, target) => {
-                target["data_vertex_colors"] = structuredClone(instance["data_vertex_colors"])
-                console.log("vertex color copied")
+                target[VERTEX_COLOR_DATA_NAME] = structuredClone(instance[VERTEX_COLOR_DATA_NAME])
+            },
+            reset: (instance) => {
+                instance_vertex_colors = {};
+                for(let vertex_key in instance.vertices) {
+                    instance_vertex_colors[vertex_key] = DEFAULT_VERTEX_COLOR
+                }
+                instance[VERTEX_COLOR_DATA_NAME] = instance_vertex_colors
             },
             export: true
         });
 
-        Blockbench.on('add_mesh', onaddmesh);
+        // MeshFace.extend updates the uv coordinates to match any changes in the vertices
+        // We need to also update the vertex colors in the same way
+        // The MeshFace constructor also uses this, to reduce code duplication (nice)
+        old_mesh_face_extend = MeshFace.prototype.extend;
+        MeshFace.prototype.extend = function(data) {
+            let old_return = old_mesh_face_extend.bind(this, data)()
+            if(old_return !== this) {
+                console.warn("[Vertex_colors]: Return value of MeshFace.extend() was unexpected. This indicates a change in Blockbench that may effect vertex colors!")
+            }
+            this.vertices.forEachReverse((key, i) => {
+                if (typeof key != 'string' || !key.length) {
+                    delete this[VERTEX_COLOR_DATA_NAME][key];
+                    return;
+                }
+                if(!this[VERTEX_COLOR_DATA_NAME]){
+                    this[VERTEX_COLOR_DATA_NAME] = {};
+                }
+                if(!this[VERTEX_COLOR_DATA_NAME][key]){
+                    this[VERTEX_COLOR_DATA_NAME][key] = DEFAULT_VERTEX_COLOR;
+                }
+                if (data[VERTEX_COLOR_DATA_NAME] && data[VERTEX_COLOR_DATA_NAME][key] instanceof Array) {
+                    this[VERTEX_COLOR_DATA_NAME][key].replace(data[VERTEX_COLOR_DATA_NAME][key]);
+                }
+            })
+            for (let key in this[VERTEX_COLOR_DATA_NAME]) {
+                if (!this.vertices.includes(key)) {
+                    delete this[VERTEX_COLOR_DATA_NAME][key];
+                }
+		    }
+            return old_return;
+        }
+
+        // Same deal with getSaveCopy, need to make sure the copy also gets the vertex color data
+        old_meshFace_getSaveCopy = MeshFace.prototype.getSaveCopy;
+        MeshFace.prototype.getSaveCopy = function(nested) {
+            let copy = old_meshFace_getSaveCopy.bind(this, nested)()
+            copy[VERTEX_COLOR_DATA_NAME] = structuredClone(this[VERTEX_COLOR_DATA_NAME])
+            return copy;
+        }
     },
     onunload() {
-        vertex_color_property.delete();
-        Blockbench.removeListener('add_mesh', onaddmesh);
+        // Reset all the monkey patches
+        MeshFace.prototype.extend = old_mesh_face_extend;
+        MeshFace.prototype.getSaveCopy = old_meshFace_getSaveCopy;
+        vertex_color_property.delete()
     }
 });
+
